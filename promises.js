6290eb2b53175040d11d473dab432e285a6068ea
indu priya <induvasu0305@gmail.com>
2022-05-17 12:54:25 +0530

    initial commit

---
+++ promises.js
@@ -0,0 +1,57 @@
+const posts = [{ title: 'Post One', body: 'This is post one'},
+               {   title: 'Post Two', body: 'This is post two'}
+   
+];
+
+function getPosts() {
+    setTimeout(() => {
+        let output = '';
+        posts.forEach((post ,index) => {
+            output += `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = output;
+    }, 1000);
+}
+
+function createPost(post){
+    return new Promise((resolve , reject) => {
+        setTimeout(() => {
+            posts.push(post);
+            const error = false;
+
+            if(!error){
+                resolve();
+            }else{
+                reject('ERROR : SOMETHING WENT WRONG');
+            }
+        }, 2000);
+    });
+   
+}
+
+// createPost({title : 'post Three', body : 'This is body three'})
+// .then(getPosts)
+// .catch(err => console.log(err));
+
+//ASYNC/AWAIT/fetch
+// async function init(){
+//     await  createPost({title: 'Post Three', body: 'This is post Three'});
+//     getPosts();     
+// }
+// init();
+async function fetchUsers(){
+    const res = await fetch ('https://jsonplaceholder.typicode.com/users');
+    const data =  await res.json();
+    console.log(data);
+}
+fetchUsers();
+
+
+
+//PROMISE ALL
+// const promise1 = Promise.resolve('Hello world');
+// const promise2 = 10;
+// const promise3 = new Promise((resolve, reject) => setTimeout(resolve, 2000, 'Goodbye'));
+// const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
+
+// Promise.all([promise1, promise2, promise3, promise4]).then(values => console.log(values));
